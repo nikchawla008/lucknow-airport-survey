ba24403602cfc9fa1aceed8528e300d02c6b7273
Nikunj Chawla <nik.chawla008@gmail.com>
2024-01-05 16:52:48 +0530

    Remove Angular material dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatStepperModule} from "@angular/material/stepper";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {MatInputModule} from "@angular/material/input";
-import {MatButtonModule} from "@angular/material/button";
-import {MatCardModule} from "@angular/material/card";
-import {MatRadioModule} from "@angular/material/radio";
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import {NgOptimizedImage, registerLocaleData} from '@angular/common';
 import en from '@angular/common/locales/en';
 import { HttpClientModule } from '@angular/common/http';
-import {NzCardModule} from "ng-zorro-antd/card";
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {NzRadioModule} from "ng-zorro-antd/radio";
 import { NzButtonModule } from 'ng-zorro-antd/button';
-import {NzSelectModule} from "ng-zorro-antd/select";
 import {NzTreeSelectModule} from "ng-zorro-antd/tree-select";
 import {NzInputNumberModule} from "ng-zorro-antd/input-number";
-import {NzSpaceModule} from "ng-zorro-antd/space";
 import {NzModalModule} from "ng-zorro-antd/modal";
+import {NzSelectModule} from "ng-zorro-antd/select";
 
 registerLocaleData(en);
 
@@ -36,15 +29,9 @@ registerLocaleData(en);
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatStepperModule,
     FormsModule,
     ReactiveFormsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatCardModule,
-    MatRadioModule,
     HttpClientModule,
-    NzCardModule,
     NzInputModule,
     NgbModule,
     NzRadioModule,
@@ -52,7 +39,6 @@ registerLocaleData(en);
     NzSelectModule,
     NzTreeSelectModule,
     NzInputNumberModule,
-    NzSpaceModule,
     NzModalModule,
     NgOptimizedImage,
   ],
